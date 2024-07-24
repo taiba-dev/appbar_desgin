b056ed4e74e0d10e180dd6ed2aa5baa7e5ef7845
taiba-dev <taibanaseem131@gmail.com>
2024-07-24 14:11:45 +0500

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,12 @@
+import 'dart:ui';
+
+import 'package:flutter/animation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/painting.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,21 +15,20 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home:const _screen1()
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const _screen1());
   }
-}class _screen1 extends StatefulWidget {
+}
+
+class _screen1 extends StatefulWidget {
   const _screen1({super.key});
 
   @override
@@ -32,19 +38,88 @@ class MyApp extends StatelessWidget {
 class __screen1State extends State<_screen1> {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar:AppBar(backgroundColor:Color.fromARGB(255, 203, 248, 3),
-      
-      title: Text(("App bar design")),
-      centerTitle: true,
-      leading:Icon(Icons.menu),
-      actions: [Icon(Icons.search),
-      Icon(Icons.notifications)],
-
-      )
-      
-      
-
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.amber,
+        title: Text(("UI/UX design")),
+        centerTitle: true,
+        leading: Icon(Icons.more_vert),
+        actions: [Icon(Icons.search), Icon(Icons.notifications)],
+      ),
+      backgroundColor: Color.fromARGB(255, 155, 102, 102),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.all(Radius.circular(10))),
+            height: 100,
+            width: 500,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                //column1
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      height: 60,
+                      width: 60,
+                      decoration: BoxDecoration(
+                        color: Colors.amber,
+                        borderRadius: BorderRadius.circular(95),
+                      ),
+                      child: Icon(
+                        Icons.notification_add_rounded,
+                        size: 30,
+                      ),
+                    ),
+                    const Text("follow"),
+                  ],
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      height: 60,
+                      width: 60,
+                      decoration: BoxDecoration(
+                        color: Colors.amber,
+                        borderRadius: BorderRadius.circular(95),
+                      ),
+                      child: Icon(
+                        Icons.message_rounded,
+                        size: 30,
+                      ),
+                    ),
+                    const Text("message"),
+                  ],
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      height: 60,
+                      width: 60,
+                      decoration: BoxDecoration(
+                        color: Colors.amber,
+                        borderRadius: BorderRadius.circular(95),
+                      ),
+                      child: Icon(
+                        Icons.favorite_rounded,
+                        size: 30,
+                      ),
+                    ),
+                    Text("favorite"),
+                  ],
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
